098fcf7575d585ec20e46b6b63f6b35127afd31a
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-10-12 20:13:25 -0600

    java

--- Fudamentos de Programación/Ejemplos/src/Ejemplo15.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo15.java
@@ -1,5 +1,13 @@
 public class Ejemplo15 {
     public static void main(String[] args) {
+        int n;
 
+        n = Utils.leerEntero("\nPorfavor Ingrese un número entero Positivo: ");
+
+        System.out.println("\nLos números desde " + n + " hasta 1, son:");
+
+        for (int i = n + 1; i > 1; i--) {
+            System.out.println(i - 1);
+        }
     }
 }
