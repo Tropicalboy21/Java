e2472dbbafe10cad56bcf6fb44069e7fb2d0fb32
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-10-12 20:00:10 -0600

    java

---
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo14.java
@@ -0,0 +1,15 @@
+public class Ejemplo14 {
+    public static void main(String[] args) {
+        int n;
+
+        n = Utils.leerEntero("\nPorfavor Ingrese un numero entero Positivo: ");
+
+        System.out.println("\nLos números pares entre 1 y 10 son:");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
