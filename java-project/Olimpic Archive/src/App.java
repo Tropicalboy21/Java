324938f4e6c68ab2a0552fc1f8c95e61e0b18e31
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-10-21 19:13:39 -0600

    java

--- java-project/Olimpic Archive/src/App.java
+++ java-project/Olimpic Archive/src/App.java
@@ -1,7 +1,16 @@
 public class App {
     public static void main(String[] args) {
-        String pais;
-        int oro, plata, bronce, puntaje;
+        String pais = "";
+        String pais1 = "";
+        String pais2 = "";
+        String pais3 = "";
+        int oro = 0;
+        int plata = 0;
+        int bronce = 0;
+        int puntaje = 0;
+        int puntaje1 = 0;
+        int puntaje2 = 0;
+        int puntaje3 = 0;
         int contador = 3;
 
         for (int i = 1; i <= contador; i++) {
@@ -12,8 +21,33 @@ public class App {
 
             puntaje = (oro * 3) + (plata * 2) + (bronce * 1);
 
-            System.out.println("\nEl puntaje total de " + pais + " es: " + puntaje);
+            if (i == 1) {
+                pais1 = pais;
+                puntaje1 = puntaje;
+            } else if (i == 2) {
+                pais2 = pais;
+                puntaje2 = puntaje;
+            } else if (i == 3) {
+                pais3 = pais;
+                puntaje3 = puntaje;
+            }
+
         }
 
+        System.out.println("\nEl puntaje total de " + pais1 + " es: " + puntaje1);
+        System.out.println("El puntaje total de " + pais2 + " es: " + puntaje2);
+        System.out.println("El puntaje total de " + pais3 + " es: " + puntaje3);
+
+        if (puntaje1 > puntaje2) {
+            if (puntaje1 > puntaje3) {
+                System.out.println("\nEl pais que obtuvo mayor puntaje es: " + pais1);
+            } else {
+                System.out.println("\nEl pais que obtuvo mayor puntaje es: " + pais3);
+            }
+        } else if (puntaje2 > puntaje3) {
+            System.out.println("\nEl pais que obtuvo mayor puntaje es: " + pais2);
+        } else {
+            System.out.println("\nEl pais que obtuvo mayor puntaje es: " + pais3);
+        }
     }
 }
