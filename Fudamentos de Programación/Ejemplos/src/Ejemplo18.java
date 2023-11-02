4e531585eaf1831858b88bcd5e32d797daa6fe5a
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-02 16:13:30 -0600

    java loops

--- Fudamentos de Programación/Ejemplos/src/Ejemplo18.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo18.java
@@ -2,11 +2,12 @@ public class Ejemplo18 {
     public static void main(String[] args) {
         int numPapitas = 0;
         int numPlatanitos = 0;
-        int precioPapitasCompra = 0;
-        int precioPlatanitosCompra = 0;
+        int precioPapitasCompra = 500;
+        int precioPlatanitosCompra = 600;
         int precioPapitasVenta = 0;
         int precioPlatanitosVenta = 0;
         int promo = 0;
+        int i = 1;
 
         numPapitas = Utils.leerEntero("\nPorfavo ingrese el numero de papitas a comprar: ");
         numPlatanitos = Utils.leerEntero("\nPorfavo ingrese el numero de platanitos a comprar: ");
@@ -17,5 +18,13 @@ public class Ejemplo18 {
 
         System.out
                 .println("\nnumero total de papitas: " + numPapitas + "\nnumero total de platanitos: " + numPlatanitos);
+
+        System.out.println("\nBienvenido al nuevo edificio universitario");
+
+        do {
+            System.out.println("piso " + i);
+            i++;
+
+        } while (i < 4);
     }
 }
