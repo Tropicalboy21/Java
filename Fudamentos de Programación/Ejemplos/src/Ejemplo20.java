034b47325dd08ee1a66bf26f92eff3e4f8aa68d1
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-02 20:46:11 -0600

    greater number

--- Fudamentos de Programación/Ejemplos/src/Ejemplo20.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo20.java
@@ -3,10 +3,26 @@ public class Ejemplo20 {
     static int numeroMayor(int a, int b, int c) {
         int mayor = a;
 
+        if (a > b) {
+            if (a > c) {
+                System.out.println("\nEl Mayor numero es: " + a);
+                mayor = a;
+            } else {
+                System.out.println("\nEl Mayor numero es: " + c);
+                mayor = c;
+            }
+        } else if (b > c) {
+            System.out.println("\nEl Mayor numero es: " + b);
+            mayor = b;
+        } else {
+            System.out.println("\nEl Mayor numero es: " + c);
+            mayor = c;
+        }
+
         return mayor;
     }
 
     public static void main(String[] args) {
-
+        numeroMayor(222, 720, 110);
     }
 }
