b7695eefb2281836be112e22590086a206e8e386
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-02 20:35:54 -0600

    new example

---
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo20.java
@@ -0,0 +1,12 @@
+public class Ejemplo20 {
+
+    static int numeroMayor(int a, int b, int c) {
+        int mayor = a;
+
+        return mayor;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
