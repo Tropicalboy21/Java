bf1189a286d28bc2d074b054d8e57b83b7b213bd
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-02 20:29:51 -0600

    refactor the code

--- Fudamentos de Programación/Ejemplos/src/Ejemplo19.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo19.java
@@ -1,11 +1,8 @@
 public class Ejemplo19 {
 
     static double metrosApulgadas(double metros) {
-        double pulgada = 39.37;
 
-        double nuevoValor = 0;
-
-        nuevoValor = metros * (pulgada / 1);
+        double nuevoValor = metros * 39.37;
 
         System.out.println("\n" + metros + " metros es igual a " + nuevoValor + " pulgadas");
 
@@ -13,11 +10,8 @@ public class Ejemplo19 {
     }
 
     static double metrosApies(double metros) {
-        double pie = 3.28;
 
-        double nuevoValor = 0;
-
-        nuevoValor = metros * (pie / 1);
+        double nuevoValor = metros * 3.28;
 
         System.out.println("\n" + metros + " metros es igual a " + nuevoValor + " pies");
 
