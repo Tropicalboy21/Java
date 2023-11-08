0946032c282d77ee5e1d5b6d4ef6384ad247a183
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-07 19:47:07 -0600

    java practice

---
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo21.java
@@ -0,0 +1,7 @@
+public class Ejemplo21 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Math.round(Math.random()*20));
+        }
+    }
+}
