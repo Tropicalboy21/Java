312644f14a6254850dff3c6b7af4c6a3e9687d00
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-09 20:43:05 -0600

    practice

--- Fudamentos de Programación/Ejemplos/src/Ejemplo22.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo22.java
@@ -1,5 +1,48 @@
 public class Ejemplo22 {
     public static void main(String[] args) {
 
+        double victorias1 = 0;
+        double victorias2 = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            String usuarioUno = Utils.leerString("\nUsuario uno : piedra, papel o tijera? ");
+
+            String usuarioDos = Utils.leerString("\nUsuario dos : piedra, papel o tijera? ");
+
+            if (usuarioUno == "tijeras" && usuarioDos == "papel") {
+                victorias1 += 1;
+            } else if (usuarioUno == "papel" && usuarioDos == "tijera") {
+                victorias2 += 1;
+            }
+
+            if (usuarioUno == "papel" && usuarioDos == "piedra") {
+                victorias1 += 1;
+            } else if (usuarioUno == "pidra" && usuarioDos == "papel") {
+                victorias2 += 1;
+            }
+
+            if (usuarioUno == "piedra" && usuarioDos == "tijeras") {
+                victorias1 += 1;
+            } else if (usuarioUno == "tijeras" && usuarioDos == "piedra") {
+                victorias2 += 1;
+            }
+
+            if (usuarioUno == "piedra" && usuarioDos == "piedra") {
+                victorias1 += 0.5;
+            } else if (usuarioUno == "papel" && usuarioDos == "papel") {
+                victorias2 += 0.5;
+            } else if (usuarioUno == "tijeras" && usuarioDos == "tijeras") {
+                victorias2 += 0.5;
+            }
+        }
+
+        if (victorias1 == 3 && victorias2 == 2) {
+            System.out.println("el ganador es el usuario uno");
+        } else if (victorias1 == 2 && victorias2 == 3) {
+            System.out.println("el ganador es el usuario dos");
+        } else if (victorias1 == 2.5 && victorias2 == 2.5) {
+            System.out.println("Es un Empate");
+        }
     }
 }
---
+++ Fudamentos de Programación/Ejemplos/src/quiz.java
@@ -0,0 +1,11 @@
+public class quiz {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x <= 5) {
+            if (x < 5) {
+                x = x + 1;
+            }
+            System.out.println(x);
+        }
+    }
+}
