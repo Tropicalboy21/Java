87a41575b577708bd328b7a9fdcaccd27e3acfd1
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-14 18:52:23 -0600

    classes

--- Fudamentos de Programación/Ejemplos/src/Arreglos.java
+++ Fudamentos de Programación/Ejemplos/src/Arreglos.java
@@ -1,7 +1,21 @@
 public class Arreglos {
-    public static void main(String[] args) {
-        int edades[] = { 12, 34, 5, 12 };
 
-        System.out.println(edades);
+    public static int sumar(int[] valores) {
+        int contador = 0;
+
+        for (int i = 0; i < valores.length; i++) {
+            contador += valores[i];
+        }
+
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        int edades[] = new int[4];
+        edades[0] = 17;
+        edades[1] = 27;
+        edades[2] = 37;
+        edades[3] = 100;
+        System.out.println(sumar(edades));
     }
 }
