f021a1f6a85a928dae887b0e90a2784965e038bb
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-15 15:27:13 -0600

    practice

--- Fudamentos de Programación/Ejemplos/src/Arreglo1.java
+++ Fudamentos de Programación/Ejemplos/src/Arreglo1.java
@@ -1,7 +1,24 @@
 public class Arreglo1 {
+    public static double sumar(double[] datos) {
+        double suma = 0;
+        for (double dato : datos) {
+            suma += dato;
+        }
+        return suma;
+    }
+
     public static void main(String[] args) {
         int cantidadEstudiantes;
         double promedio;
         double[] notas = null;
+
+        cantidadEstudiantes = Utils.leerEntero("\nDigite la candidad de estudiantes: ");
+        notas = new double[cantidadEstudiantes];
+
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = Utils.leerDoble("Digite la nota del estudiante " + (i + 1) + ":");
+
+            promedio = sumar(notas) / notas.length;
+        }
     }
 }
