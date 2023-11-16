fa1ea2c427a89791fd48306e909b9f23810ac0d3
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-15 21:22:29 -0600

    practice

---
+++ Fudamentos de Programación/Ejemplos/src/Ventas.java
@@ -0,0 +1,27 @@
+public class Ventas {
+    public static double ventaMayor(double arr[]) {
+        int i;
+
+        // Initialize maximum element
+        double max = arr[0];
+
+        // Traverse array elements from second and
+        // compare every element with current max
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = i;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        double[] ventas = new double[12];
+
+        for (int i = 0; i < ventas.length; i++) {
+            ventas[i] = Utils.leerDoble("\nDigite las ventas del mes " + (i + 1) + ":");
+        }
+
+        System.out.printf("\nEl mes con más ventas fue : " + ventaMayor(ventas));
+    }
+}
