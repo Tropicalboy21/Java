6c4931b9b21bf715d9633e8445bb5c805c05edbe
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-23 20:08:20 -0600

    class

--- Fudamentos de Programación/Ejemplos/src/Ejemplo28.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo28.java
@@ -1,13 +1,13 @@
 public class Ejemplo28 {
+
     public static void main(String[] args) {
 
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == arr[0]) {
-                arr[i] = arr[7];
-                arr[7] = arr[0];
-            }
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            arr[n - 1] = arr[i];
 
         }
 
