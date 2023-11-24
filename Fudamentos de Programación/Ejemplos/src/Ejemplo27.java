c8595c23022582e4abb5d8ab09ce6d4f25604c96
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-23 18:55:46 -0600

    class

---
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo27.java
@@ -0,0 +1,13 @@
+public class Ejemplo27 {
+    public static void main(String[] args) {
+        String frase = "Luz ilumina nuestro dia";
+
+        // System.out.println(name.replaceAll("\\s", ""));
+
+        char[] palabraArr = frase.toCharArray();
+
+        for (int i = 0; i < 1; i++) {
+            System.out.println(palabraArr);
+        }
+    }
+}
