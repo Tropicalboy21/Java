90e090c4d8a57dcd54cb64094ce63462ad560048
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-28 19:35:00 -0600

    class

--- Fudamentos de Programación/Ejemplos/src/Ejemplo28.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo28.java
@@ -1,5 +1,4 @@
 public class Ejemplo28 {
-
     public static void invertir(int arr[], int n) {
         for (int i = 0; i < n / 2; i++) {
             int temp = arr[i];
@@ -12,16 +11,12 @@ public class Ejemplo28 {
         for (int j = 0; j < arr.length; j++) {
             System.out.println(arr[j]);
         }
-
     }
 
     public static void main(String[] args) {
-
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
         int n = arr.length;
-
         invertir(arr, n);
         imprimir(arr);
-
     }
 }
