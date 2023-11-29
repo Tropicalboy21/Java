e31e1a6b1095e70736e5f76cf0b906abc4b254b0
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-28 20:30:20 -0600

    practice class

--- Fudamentos de Programación/Ejemplos/src/Ejemplo31.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo31.java
@@ -1,14 +1,18 @@
 import java.util.Arrays;
 
 public class Ejemplo31 {
-    public static void ordenar(int[] edades, int[] compras) {
+    public static void ordenar(int[] edades, double[] compras) {
         Arrays.sort(edades);
-
+        Arrays.sort(compras);
     }
 
     public static void main(String[] args) {
         int[] edades = { 25, 45, 32, 82, 18, 70, 65, 46, 17, 20 };
         double[] compras = { 125.0, 125.81, 450.2, 125.2, 140.9, 250.4, 380.5, 145.11, 780.0, 450.4 };
 
+        ordenar(edades, compras);
+
+        System.out.println(Arrays.toString(edades));
+        System.out.println(Arrays.toString(compras));
     }
 }
