1312edf6a152cd20c73d9789df52e4976e4e86ef
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-28 20:23:24 -0600

    class

--- Fudamentos de Programación/Ejemplos/src/Ejemplo31.java
+++ Fudamentos de Programación/Ejemplos/src/Ejemplo31.java
@@ -1,3 +1,14 @@
 public class Ejemplo31 {
+    public static int ordenar(int[]arr){
 
+        Arrays.sort()
+
+        return ;
+    }
+
+    public static void main(String[] args) {
+        int[] edades = { 25, 45, 32, 82, 18, 70, 65, 46, 17, 20 };
+        double[] compras = { 125.0, 125.81, 450.2, 125.2, 140.9, 250.4, 380.5, 145.11, 780.0, 450.4 };
+
+    }
 }
