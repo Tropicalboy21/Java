ad02f8e4a208ca0da3dbacef59a95b10a354fdec
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-30 18:33:59 -0600

    practice

---
+++ Fudamentos de Programación/Ejemplos/src/Practica04.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Practica04 {
+    // public static void llenarNumeroAleatorios(double[] datos, int min, int max) {
+    // long semilla = 1234567890l;
+    // Random random = new Random(semilla);
+    // for (int i = 0; i < datos.length; i++) {
+    // datos[i] = random.nextLong(min, max);
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // double[] numeros = new double[20];
+    // llenarNumeroAleatorios(numeros, 100, 500);
+    // System.out.println(Arrays.toString(numeros));
+    // }
+}
