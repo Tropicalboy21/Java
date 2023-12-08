b4f2a1c7d813e59bb76ca2b71347b0a0c516a9d3
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-12-08 08:30:44 -0600

    pracatice

---
+++ Fudamentos de Programación/Ejemplos/src/prueba.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class prueba {
+    public static void main(String args[]) {
+        System.out.println(sumar);
+    }
+
+    static void sumar() {
+        int notas[] = { 30, 20, 100 };
+        for (int i = 0; i < notas.length; i++) {
+            int suma = notas[0];
+        }
+        return suma;
+    }
+
+}
