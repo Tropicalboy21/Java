0069a68c241aad5156fdb1f496d6c91b90049525
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-05 19:27:56 -0600

    practice

--- OOP/agencia/src/Vehiculo.java
+++ OOP/agencia/src/Vehiculo.java
@@ -1,3 +1,31 @@
 public class Vehiculo {
+    // 1. Atributos
+    private String marca;
+    private int cantidad;
+    private String contacto;
 
+    // 2. Métodos de acceso get y set
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
 }
