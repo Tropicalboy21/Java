5a4b40fa4538c085f315b6697b08bcdb2f06d1f2
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-17 11:31:03 -0600

    practice

---
+++ POO/Clothing/src/ucenfotec/ac/cr/bl/Cliente.java
@@ -0,0 +1,71 @@
+package ucenfotec.ac.cr.bl;
+
+public class Cliente {
+    private String nombre;
+    private  String primerApellido;
+    private String segundoApellido;
+    private String direccion;
+    private String correo;
+
+
+    Cliente(){};
+
+    Cliente(String nombre, String primerApellido, String segundoApellido, String direccion, String correo){
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", primerApellido='" + primerApellido + '\'' +
+                ", segundoApellido='" + segundoApellido + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
