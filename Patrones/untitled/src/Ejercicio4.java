0046f8c3e932b8fc28ede938c1c724c6755ee705
Lenin Ugalde <ugaldelenin@hotmail.com>
2025-01-24 16:14:59 -0600

    Practice

--- Patrones/untitled/src/Ejercicio4.java
+++ Patrones/untitled/src/Ejercicio4.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Ejercicio4 {
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        System.out.print("\nIngrese un numero: ");
+        numero = sc.nextInt();
+
+        boolean primo = esPrimo(numero);
+        boolean fibonacci = esFibonacci(numero);
+        boolean par = esPar(numero);
+
+        StringBuilder resultado = new StringBuilder();
+        resultado.append(numero);
+
+        if (primo) {
+            resultado.append(" es primo,");
+        } else {
+            resultado.append(" no es primo");
+        }
+        if (fibonacci) {
+            resultado.append(" es fibonacci,");
+        } else {
+            resultado.append(" no es fibonacci");
+        }
+        if (par) {
+            resultado.append(" y es par.");
+        } else {
+            resultado.append(" y es impar.");
+        }
+
+        System.out.println(resultado);
+    }
+
+    public static boolean esPrimo(int num){
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean esPar(int num){
+        return num % 2 == 0;
+    }
+
+    public static boolean esFibonacci(int num) {
+        return isPerfectSquare(5 * num * num + 4) || isPerfectSquare(5 * num * num - 4);
+    }
+
+    public static boolean isPerfectSquare(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        return (sqrt * sqrt == num);
+    }
+
 }
--- Patrones/untitled/src/Ejercicio5.java
+++ Patrones/untitled/src/Ejercicio5.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Ejercicio5 {
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calculateAge();
+    }
+
+    public static void calculateAge(){
+        int y = 0;
+        int m = 0;
+        int d = 0;
+        LocalDate currentDate = LocalDate.now();
+        int years = 0;
+
+        System.out.print("Porfavor ingrese su año de nacimiento: ");
+        y = sc.nextInt();
+
+        System.out.print("Porfavor ingrese su mes de nacimiento: ");
+        m = sc.nextInt();
+
+        System.out.print("Porfavor ingrese su dia de nacimiento: ");
+        d = sc.nextInt();
+
+        LocalDate birthDay = LocalDate.of(y,m,d);
+
+        years =
+                currentDate.getYear() - birthDay.getYear();
+
+        System.out.println("\nFecha actual:" + currentDate);
+        System.out.println("\nFecha de nacimiento: " + birthDay);
+        System.out.println("\nHan transcurrido " + years + " años entre ambas fechas." );
+
+
+    }
 }
