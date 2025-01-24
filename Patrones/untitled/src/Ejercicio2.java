76c9c648799a4854c40a0d1066182269593ce9fe
Lenin Ugalde <ugaldelenin@hotmail.com>
2025-01-24 13:45:08 -0600

    Practice

--- Patrones/untitled/src/Ejercicio2.java
+++ Patrones/untitled/src/Ejercicio2.java
@@ -1,3 +1,95 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Ejercicio2 {
-    
+    static Scanner sc = new Scanner(System.in);
+    static double balance = 0.00;
+
+    public static void main(String[] args) {
+        Account();
+    }
+
+    public static void Account(){
+        int option = -1;
+
+        do {
+            System.out.println("\nTrasacciones de cuenta.");
+            System.out.println("\nSaldo actual: ₡" + balance);
+            System.out.println("\n----- Menu -----");
+            System.out.println("1. Depósito");
+            System.out.println("2. Retiro");
+            System.out.println("3. Salir");
+            System.out.print("ingrese una opción: ");
+            option = sc.nextInt();
+            processOption(option);
+        } while (option != 3);
+    }
+
+
+    public static void processOption(int option){
+        switch(option){
+            case 1 : CashIn();
+                break;
+            case 2 : CashOut();
+                break;
+            case 3 :
+                System.out.println("gracias por su visita");
+                break;
+            default:
+                System.out.println("opcion invalida");
+        }
+    }
+
+
+    public static void CashIn(){
+
+        double amount = 0.00;
+        int choice = 0;
+
+        System.out.println("\nDeposito");
+        System.out.println("\nPor favor ingrese el monto a depositar: ");
+        amount = sc.nextInt();
+
+        balance += amount;
+
+        System.out.println("\nMonto Depositado con exito!");
+        System.out.println("\nSaldo Actual: ₡" + balance);
+
+
+        System.out.print("\nDesea realizar otra transacción? digite 1 para si y 2 para No: ");
+        choice = sc.nextInt();
+
+        if (choice == 2){
+            System.out.println("gracias por su visita");
+            System.exit(0);
+        }
+
+    }
+
+    public static void CashOut(){
+        double amount = 0.00;
+        int choice = 0;
+
+        System.out.println("\nRetiro");
+        System.out.println("\nPor favor ingrese el monto a retirar: ");
+        amount = sc.nextInt();
+
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("\nMonto retirado con exito!");
+            System.out.println("\nSaldo Actual despues del retiro: ₡" + balance);
+
+        } else {
+            System.out.println("\nLo sentimos fondos insuficientes en la cuenta");
+        }
+
+        System.out.print("\nDesea realizar otra transacción? digite 1 para si y 2 para No: ");
+        choice = sc.nextInt();
+
+        if (choice == 2) {
+            System.out.println("gracias por su visita");
+            System.exit(0);
+        }
+    }
 }
--- Patrones/untitled/src/Ejercicio3.java
+++ Patrones/untitled/src/Ejercicio3.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Ejercicio3 {
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Circumference();
+    }
+
+    public static void  Circumference(){
+
+        System.out.println("\nIngrese 5 radios de circulos");
+
+        double[] values = new double[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("\nIngrese el radio " + (i + 1)+ ":");
+            values[i] = sc.nextDouble();
+        }
+
+        System.out.println("\nLos valores de los radios ingresados son: ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("| Valor "+ (i+1)+":" + values[i] + "| Diametro:" + (values[i] * 2) + "| Circunferencia: " +  ((values[i] * 2) * 3.1416) + "|");
+        }
+
+    }
 }
