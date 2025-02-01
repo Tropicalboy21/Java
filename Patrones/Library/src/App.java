e0e8c3606ba25beba41e6949b18b4bd8f1eaa0b5
Lenin Ugalde <ugaldelenin@hotmail.com>
2025-01-31 20:34:25 -0600

    Practice

--- Patrones/Library/src/App.java
+++ Patrones/Library/src/App.java
@@ -12,9 +12,9 @@ public class App {
             System.out.printf("     Bienvenido a la Biblioteca     %n");
             System.out.printf("                 Menu               %n");
             System.out.printf("------------------------------------%n");
-            System.out.printf("  1.   Registrar usuario            %n");
+            System.out.printf("  1.   Listar libros                %n");
             System.out.printf("  2.   Libros prestados por usuario %n");
-            System.out.printf("  3.   Registrar libro              %n");
+            System.out.printf("  3.   prestar libro                %n");
             System.out.printf("  4.   Listar libros                %n");
             System.out.printf("  5.   prestar libro                %n");
             System.out.printf("  6.   Salir                        %n");
